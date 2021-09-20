2c795e0606546497f0b476b099e4e947ce591a18
Lyubimova Olga <olga.lyubimova.03@mail.ru>
2021-09-20 18:15:15 +0300

    lab3

---
+++ lab3/a1.py
@@ -0,0 +1,18 @@
+
+from graph import *
+
+penColor("black")
+penSize(2)
+brushColor("yellow")
+circle(200, 200, 100)
+brushColor("red")
+circle(160, 175, 15)
+circle(240, 175, 13)
+brushColor("black")
+circle(160, 175, 7)
+circle(240, 175, 6)
+penSize(15)
+line(190,164, 100, 120)
+line(215, 165, 300, 110)
+rectangle(160,240, 240,250)
+run()
