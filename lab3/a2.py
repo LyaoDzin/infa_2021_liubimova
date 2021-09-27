e43dd5eb2bb92d81e008da69f80ef6d67beb57f4
Lyubimova Olga <olga.lyubimova.03@mail.ru>
2021-09-27 16:22:40 +0300

    lab3>

---
+++ lab3/a2.py
@@ -0,0 +1,101 @@
+from graph import *
+
+a = 150
+b = 90
+
+brushColor(154,154,154)
+rectangle(20,20,500,800)
+brushColor("black")
+rectangle(20, 300, 500,700)
+
+
+brushColor( 154, 154, 154)
+brushColor("white")
+penColor("white")
+circle(430,100,50)
+
+brushColor(48,48,48)
+penColor(48,48,48)
+oval(520,220, 280,260)
+
+brushColor(54,46, 0)
+penColor(54,46,0)
+rectangle(60,170,300,520)
+
+brushColor(44,18,0)
+penColor(44,18,0)
+rectangle(80,410, 130,480)
+rectangle(150, 410, 200, 480)
+
+brushColor(220,153,0)
+penColor(220,153,0)
+rectangle(220,410,270,480)
+
+brushColor(154,154,99)
+penColor(154,154,99)
+rectangle(70,170,90,290)
+rectangle(110,170,130,290)
+rectangle(150,170,170,290)
+rectangle(190,170,210,290)
+rectangle(230,170,250,290)
+rectangle(270,170,290,290)
+
+brushColor(48,48,48)
+penColor(48,48,48)
+rectangle(30,290,330,330)
+rectangle(40,250,320,260)
+rectangle(35,250,45,290)
+rectangle(80,250,90,290)
+rectangle(125,250,135,290)
+rectangle(160,250,170,290)
+rectangle(205,250,215,290)
+rectangle(250,250,260,290)
+rectangle(305,250,315,290)
+
+rectangle(150,160,160,127)
+
+brushColor("black")
+penColor("black")
+polygon([(20,170),(80,145), (290,145), (340,170)])
+
+brushColor(48,48,48)
+penColor(48,48,48)
+rectangle(80,160,90,125)
+
+brushColor(102,102,102)
+penColor(102,102,102)
+oval(100,100,380,135)
+
+brushColor(84, 83, 83)
+penColor(84, 83, 83)
+oval(220, 80, 470, 115)
+oval(350,140,600,160)
+
+brushColor(222, 222, 222)
+penColor(222, 222, 222)
+circle(400,500,20)
+polygon([(380,503), (360,540), (390, 560), (420, 550), (450, 520), (425, 503), (420,503), (420,503), (380,503) ])
+circle(384,515,10)
+circle(363, 538,3)
+circle(376,530,10)
+circle(386,550,10)
+circle(415,543,10)
+circle(443,523,7)
+circle(420,503,3)
+brushColor(97, 192, 232)
+penColor(97, 192, 232)
+circle(390,500,4)
+circle(406,496,4)
+brushColor("black")
+circle(389,500,2)
+circle(405,496,2)
+
+brushColor("white")
+penColor("white")
+
+line(389,499,392,497)
+line(405,495,408,493)
+
+
+
+run()
