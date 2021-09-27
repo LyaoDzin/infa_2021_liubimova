c67ab025d95092c46a383e806c0b7415d8a9b377
Lyubimova Olga <olga.lyubimova.03@mail.ru>
2021-09-27 17:22:55 +0300

    lab3>

---
+++ lab3/a3.py
@@ -0,0 +1,137 @@
+from graph import *
+
+
+
+brushColor(154,154,154)
+rectangle(20,20,500,800)
+brushColor("black")
+rectangle(20, 300, 500, 700)
+
+
+brushColor( 154, 154, 154)
+brushColor("white")
+penColor("white")
+circle(430,100,50)
+
+brushColor(48,48,48)
+penColor(48,48,48)
+oval(520,220, 280,260)
+
+
+brushColor(84, 83, 83)
+penColor(84, 83, 83)
+oval(220, 80, 470, 115)
+oval(350,140,600,160)
+
+
+brushColor(102,102,102)
+penColor(102,102,102)
+oval(100,100,380,135)
+
+brushColor(54,46, 0)
+penColor(54,46,0)
+rectangle(480,180,360,340)
+rectangle(335,240, 220, 390)
+rectangle(60,430,180, 260)
+
+brushColor(44,18,0)
+penColor(44,18,0)
+rectangle(370,330,395,295)
+rectangle(405,330,430,295)
+rectangle(230,380, 255,345)
+rectangle( 265, 380, 290,345)
+rectangle(70,420,95,385)
+rectangle(105,420, 130, 385)
+
+brushColor(220,153,0)
+penColor(220,153,0)
+rectangle(440, 330, 465, 295)
+rectangle(300, 380, 325, 345)
+rectangle(140, 420, 165, 385)
+
+brushColor(154,154,99)
+penColor(154,154,99)
+
+rectangle(365,180, 375,240)
+rectangle(390, 180,400,240)
+rectangle(415,180, 425,240)
+rectangle(440,180,450,240)
+rectangle(465,180,475,240)
+
+rectangle(225, 240, 235, 320)
+rectangle(250,240, 260,320)
+rectangle(275,240,285,320)
+rectangle(300,240, 310, 320)
+rectangle(320,240, 330,320)
+
+rectangle(65,260, 75, 340)
+rectangle(90,260,100, 340)
+rectangle(115, 260, 125, 340)
+rectangle(140, 260, 150,340)
+rectangle(165, 260, 175, 340)
+
+
+
+brushColor(48,48,48)
+penColor(48,48,48)
+rectangle(355, 235, 485, 245)
+rectangle(360, 220, 480, 225)
+rectangle(360, 225, 370, 235)
+rectangle(385, 225, 395, 235)
+rectangle(410, 225, 420, 235)
+rectangle(435, 225, 445, 235)
+rectangle(460, 225, 470, 235)
+rectangle(475, 225, 485, 235)
+
+
+rectangle(215,320, 340, 310)
+rectangle(220, 300, 335, 295)
+rectangle(220, 295, 230, 300)
+rectangle(245, 295, 255, 300)
+rectangle(270, 295, 280, 300)
+rectangle(295, 295, 305, 300)
+rectangle(320, 295, 330, 300)
+
+rectangle(55, 330, 185, 340)
+rectangle(60, 310, 180, 315)
+
+
+
+brushColor("black")
+penColor("black")
+polygon([(20,170),(80,145), (290,145), (340,170)])
+
+brushColor(48,48,48)
+penColor(48,48,48)
+rectangle(80,160,90,125)
+
+
+
+brushColor(222, 222, 222)
+penColor(222, 222, 222)
+circle(400,500,20)
+polygon([(380,503), (360,540), (390, 560), (420, 550), (450, 520), (425, 503), (420,503), (420,503), (380,503) ])
+circle(384,515,10)
+circle(363, 538,3)
+circle(376,530,10)
+circle(386,550,10)
+circle(415,543,10)
+circle(443,523,7)
+circle(420,503,3)
+brushColor(97, 192, 232)
+penColor(97, 192, 232)
+circle(390,500,4)
+circle(406,496,4)
+brushColor("black")
+circle(389,500,2)
+circle(405,496,2)
+
+brushColor("white")
+penColor("white")
+
+line(389,499,392,497)
+line(405,495,408,493)
+
+
+
+run()
